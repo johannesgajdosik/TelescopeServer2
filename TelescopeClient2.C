cf7b115aec37507ff14bc7bc1ee236425736b014
Johannes Gajdosik <glunatic.johannes.gajdosik@gmail.com>
2023-12-18 00:01:03 +0100

    TelescopeClient2: allow Ra/Dec in h/d-min-secs

--- TelescopeClient2.C
+++ TelescopeClient2.C
@@ -31,7 +31,7 @@ static void HandleMsgCurrentPosition(unsigned int ra_int,int dec_int) {
             << std::endl;
 }
 
-static void SendMspGoto(unsigned int ra_int,int dec_int) {
+static void SendMsgGoto(unsigned int ra_int,int dec_int) {
   unsigned char msg[24];
   *reinterpret_cast<unsigned short*>(&msg) = 24;
   *reinterpret_cast<unsigned short*>(&msg[2]) = 0;
@@ -62,11 +62,98 @@ static int HandleStellariumTelescopeProtocolServerMsg(
   return length;
 }
 
+int ParseUInt(char *&str) {
+  if (!str) return -1;
+  if (!*str) return -2;
+  if (*str < '0' || *str > '9') return -3;
+  int rval = *str++ - '0';
+  while (*str >= '0' && *str <= '9') {
+    rval = 10*rval + (*str++ - '0');
+  }
+  return rval;
+}
+
+static
+int ScanDorHMS(char *str,char d_or_h) {
+  if (!str) {
+std::cout << "E1" << std::endl;
+    return std::numeric_limits<int>::min();
+  }
+  int sign = 1;
+  if (*str == '+') str++;
+  else if (*str == '-') {sign = -1;str++;}
+  if (d_or_h=='h' && sign < 0) {
+std::cout << "E2" << std::endl;
+    return std::numeric_limits<int>::min();
+  }
+  const int h = ParseUInt(str);
+  if (h < 0) return std::numeric_limits<int>::min();
+  if ((d_or_h=='h' && h>=24) || (d_or_h=='d' && h>=90)) {
+std::cout << "E3" << std::endl;
+    return std::numeric_limits<int>::min();
+  }
+  if (*str == '\0') return sign*h*3600;
+  if (*str != d_or_h) {
+std::cout << "E4" << std::endl;
+    return std::numeric_limits<int>::min();
+  }
+  str++;
+  if (*str == '\0') return sign*h*3600;
+  const int m = ParseUInt(str);
+  if (m < 0) {
+std::cout << "E5" << std::endl;
+    return std::numeric_limits<int>::min();
+  }
+  if (h==90 && m > 0) {
+std::cout << "E6" << std::endl;
+    return std::numeric_limits<int>::min();
+  }
+  if (*str == '\0') return sign*(h*3600+m*60);
+  if (*str != 'm') {
+std::cout << "E7" << std::endl;
+    return std::numeric_limits<int>::min();
+  }
+  str++;
+  if (*str == '\0') return sign*(h*3600+m*60);
+  const int s = ParseUInt(str);
+  if (s < 0) {
+std::cout << "E8" << std::endl;
+    return std::numeric_limits<int>::min();
+  }
+  if (h==90 && s > 0) {
+std::cout << "E9" << std::endl;
+    return std::numeric_limits<int>::min();
+  }
+  if (*str == '\0') {
+    return sign*(h*3600+m*60+s);
+  }
+  if (*str != 's') {
+std::cout << "E10" << std::endl;
+    return std::numeric_limits<int>::min();
+  }
+  str++;
+  if (*str == '\0') return sign*(h*3600+m*60+s);
+std::cout << "E11" << std::endl;
+  return std::numeric_limits<int>::min();
+}
+
+static inline
+int ScanHMS(char *str) {
+  return ScanDorHMS(str,'h');
+}
+
+static inline
+int ScanDMS(char *str) {
+  return ScanDorHMS(str,'d');
+}
+
+
 int main(int argc,char *argv[]) {
   int port;
   if (argc < 3 || !(std::istringstream(argv[2])>>port) ||
       port <= 0 || port >= 0x10000) {
-    std::cerr << "Usage: " << argv[0] << " <host> <port> [<ra> <dec>]"
+    std::cerr << "Usage:   " << argv[0] << " <host> <port> [<ra> <dec>]\n"
+                 "Example: " << argv[0] << " localhost 10000 22h42m54s 56d40m06s"
               << std::endl;
     return 1;
   }
@@ -85,17 +172,28 @@ int main(int argc,char *argv[]) {
                                 &HandleStellariumTelescopeProtocolServerMsg),
                               TcpIpConnection::ErrorHandler());
   if (argc >= 5) {
-    double ra,dec;
-    if ((std::istringstream(argv[3])>>ra) &&
-        (std::istringstream(argv[4])>>dec)) {
-      const unsigned int ra_int = ra * ( 2147483648.0 / 12);
-      const int dec_int = dec * ( 2147483648.0 / 180);
-      SendMspGoto(ra_int,dec_int);
+    int ra = ScanHMS(argv[3]);
+    int dec = ScanDMS(argv[4]);
+    if (ra != std::numeric_limits<int>::min() &&
+        dec != std::numeric_limits<int>::min()) {
+      const unsigned int ra_int = (((long long int)ra)<<32) / (24*3600);
+      const unsigned int dec_int = (((long long int)dec)<<32) / (360*3600);
+      SendMsgGoto(ra_int,dec_int);
+    } else {
+      std::cerr << "<ra> <dec> must be given like 13h2m12s -90d00m00s"
+              << std::endl;
+      return 1;
     }
   }
 
 //  try {
-    io_context.run();
+    if (argc >= 5) {
+      io_context.run_for(std::chrono::seconds(10));
+    } else {
+      io_context.run();
+    }
+
+
 //  } catch (std::exception& e) {
 //    std::cerr << "Exception: " << e.what() << "\n";
 //  }
